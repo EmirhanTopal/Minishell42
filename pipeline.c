4135ae24fbed45ec28ad254e4ca65b9cdc06b562
Emirhan Topal <emirhantopal03@hotmail.com>
2025-07-15 14:09:04 +0300

    multiple pipe < 25 line changed & working

--- pipeline.c
+++ pipeline.c
@@ -1,5 +1,45 @@
 #include "minishell.h"
 
+static	void	pipeline_child_process(t_command *tmp, int *fd, int prev_fd)
+{
+	if (prev_fd != -1) // örneğin ls grep wc komutları arasında 2. komuttaysak prev_fd = fd[0] olur. 
+	{
+		dup2(prev_fd, STDIN_FILENO);
+		close(prev_fd);
+	}
+	if (tmp->next) // eğer tek komut değilse birden fazla komut varsa
+	{
+		close(fd[0]); // pipe okuma ucunu kapat çünkü yazma yapacağım pipe ın read ucu ile işim yok.
+		dup2(fd[1], STDOUT_FILENO); // fd[1] artık stdout un bir kopyası yani bundan sonra stdout a yönlendirilenler fd[1] e yönlendirilecek.
+		close(fd[1]); // pipe yazma ucunu kapat
+	}
+}
+
+static	void	pipeline_parent_process(t_command *tmp, int *fd, int *prev_fd)
+{
+	if ((*prev_fd) != -1) // eğer parent da kapatma işlemi yapılmazsa ve bir sonraki komut yok ise. fd[0] açık kalacağı için read bekler, eğer zaten bir sonraki komut var ise tekrardan pref_fd yi güncelleriz.
+		close((*prev_fd));
+	if (tmp->next)
+	{
+		(*prev_fd) = fd[0];
+		close(fd[1]);
+	}
+}
+
+static	void	pipeline_error()
+{
+	perror("fork");
+	exit(1);
+}
+
+static	void	pipeline_execute(t_command *tmp, t_shell *shell, char **envp)
+{
+	if (tmp->is_builtin)
+		execute_builtin_command(tmp, shell);
+	else
+		execute_not_builtin_command(tmp, envp);
+}
+
 void	execute_pipeline(t_command *cmd, t_shell *shell, char **envp)
 {
     int			fd[2];
@@ -16,37 +56,15 @@ void	execute_pipeline(t_command *cmd, t_shell *shell, char **envp)
 		pid = fork();
 		if (pid == 0)
 		{
-			if (prev_fd != -1) // örneğin ls grep wc komutları arasında 2. komuttaysak prev_fd = fd[0] olur. 
-			{
-				dup2(prev_fd, STDIN_FILENO);
-				close(prev_fd);
-			}
-			if (tmp->next) // eğer tek komut değilse birden fazla komut varsa
-			{
-				close(fd[0]); // pipe okuma ucunu kapat çünkü yazma yapacağım pipe ın read ucu ile işim yok.
-				dup2(fd[1], STDOUT_FILENO); // fd[1] artık stdout un bir kopyası yani bundan sonra stdout a yönlendirilenler fd[1] e yönlendirilecek.
-				close(fd[1]); // pipe yazma ucunu kapat
-			}
-			if (tmp->is_builtin)
-				execute_builtin_command(tmp, shell);
-			else
-				execute_not_builtin_command(tmp, envp);
+			pipeline_child_process(tmp, fd, prev_fd);
+			pipeline_execute(tmp, shell, envp);
 			exit(g_last_exit_status);
 		}
 		else if (pid < 0)
-		{
-			perror("fork");
-			exit(1);
-		}
+			pipeline_error();
 		//parent
-		if (prev_fd != -1) // eğer parent da kapatma işlemi yapılmazsa ve bir sonraki komut yok ise. fd[0] açık kalacağı için read bekler, eğer zaten bir sonraki komut var ise tekrardan pref_fd yi güncelleriz.
-			close(prev_fd);
-		if (tmp->next)
-		{
-			prev_fd = fd[0];
-			close(fd[1]);
-		}
+		pipeline_parent_process(tmp, fd, &prev_fd);
 		tmp = tmp->next;
 	}
-	while (wait(NULL) > 0);
+	while (wait(NULL) > 0); // wait() çağrısı, bir child process bitince onun PID’sini döner. Eğer bekleyecek child kalmadıysa -1 döner.
 }
